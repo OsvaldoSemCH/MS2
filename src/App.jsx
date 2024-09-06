a16ebf74b21584e3af818565c876c092d27d389d
OsvaldoSemCH <eduardohenriqueribeirobrasil@gmail.com>
2024-09-06 14:41:46 -0300

    Melhorada a exibição dos cards da API

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
             <div>
                <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)}/>
             </div>
-            <div>
+            <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'stretch', justifyContent: 'center', gap: '24px'}}>
             {data.map((item) => { 
              return(
               <div key={item.id}>
