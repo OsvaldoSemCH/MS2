6ad59c035d737c748061c3594f399362068e66e0
OsvaldoSemCH <eduardohenriqueribeirobrasil@gmail.com>
2024-09-06 14:23:57 -0300

    props do Card desestruturado

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import style from './Card.module.css'
 
 /* eslint-disable react/prop-types */
-export const Card = (props) => {
+export const Card = ({name, desc, value, image, category, status}) => {
   return(
       <div className={style.Card}>
-        <div className={style.Status} style={props.status ? {backgroundColor: '#00c000'} : {backgroundColor: '#c00000'}}></div>
-          <p>Álbum: <h1>{props.name}</h1></p>
-          <p>Artista: <h2>{props.desc}</h2></p>
-          <p>Preço: R$ {props.value}</p>
+        <div className={style.Status} style={status ? {backgroundColor: '#00c000'} : {backgroundColor: '#c00000'}}></div>
+          <p>Álbum: <h1>{name}</h1></p>
+          <p>Artista: <h2>{desc}</h2></p>
+          <p>Preço: R$ {value}</p>
           <div className={style.CardBottom}>
-            <img src={props.image} alt={props.name} width={150} height={"auto"}/>
-            <p>Gênero: {props.category}</p>
+            <img src={image} alt={name} width={150} height={"auto"}/>
+            <p>Gênero: {category}</p>
           </div>
       </div>
   )
