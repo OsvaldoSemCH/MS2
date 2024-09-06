ca4eeac137fa72dbf7983c37fc01f9b9a43fdf9e
OsvaldoSemCH <eduardohenriqueribeirobrasil@gmail.com>
2024-09-06 15:10:01 -0300

    Melhorada a exibição dos cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,10 +7,12 @@ export const Card = ({name, desc, value, image, category, status}) => {
         <div className={style.Status} style={status ? {backgroundColor: '#00c000'} : {backgroundColor: '#c00000'}}></div>
           <p>Álbum: <h1>{name}</h1></p>
           <p>Artista: <h2>{desc}</h2></p>
-          <p>Preço: R$ {value}</p>
           <div className={style.CardBottom}>
             <img src={image} alt={name} width={150} height={"auto"}/>
-            <p>Gênero: {category}</p>
+            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
+              <p>Preço: R$ {value}</p>
+              <p>Gênero: {category}</p>
+            </div>
           </div>
       </div>
   )
